3adaf0e816dfc4261a6253f0dd766514054d1893
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2020-04-24 12:23:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
-const cells = 3;
+const cells = 15;
 const width = 600;
 const height = 600;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y=0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -22,10 +23,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+  Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls);
 
@@ -150,4 +151,41 @@ verticals.forEach((row, rowIndex) => {
     );
     World.add(world, wall);
   });
+});
+
+// Goal
+
+const goal = Bodies.rectangle(
+  width - unitLength / 2,
+  height - unitLength / 2,
+  unitLength * 0.7,
+  unitLength * 0.7,
+  {
+    isStatic: true
+  }
+);
+World.add(world, goal);
+
+// Ball
+
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+  const {x,y} = ball.velocity;
+  if (event.keyCode === 87) {
+    Body.setVelocity(ball, {x: x, y:  - 5});
+  }
+
+  if (event.keyCode === 68) {
+    Body.setVelocity(ball, {x: x + 5, y});
+  }
+
+  if (event.keyCode === 83) {
+    Body.setVelocity(ball, {x: x, y:  + 5});
+  }
+
+  if (event.keyCode === 65) {
+    Body.setVelocity(ball, {x: x-5, y});
+  }
 });
